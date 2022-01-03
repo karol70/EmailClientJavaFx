68374be52ad1b96725f14ed5d8f30fde20921d86
Karol Węgrzyn <karolwg19@gmail.com>
2022-01-03 13:38:47 +0100

    setup java email

--- src/com/barosanu/controller/LoginWindowController.java
+++ src/com/barosanu/controller/LoginWindowController.java
@@ -1,11 +1,15 @@
 package com.barosanu.controller;
 
 import com.barosanu.EmailManager;
+import com.barosanu.controller.services.LoginService;
+import com.barosanu.model.EmailAccount;
 import com.barosanu.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import javax.mail.NoSuchProviderException;
 
 public class LoginWindowController extends BaseController {
 
@@ -23,11 +27,35 @@ public class LoginWindowController extends BaseController {
     }
 
     @FXML
-    void loginButtonAction() {
-        System.out.println("loginButtonAction!");
-        viewFactory.showMainWindow();
-        Stage stage = (Stage)errorLabel.getScene().getWindow();
-        viewFactory.closeStage(stage);
+    void loginButtonAction() throws NoSuchProviderException {
+        System.out.println("loginButtonAction!!");
+        if (fieldsAreValid()) {
+            EmailAccount emailAccount = new EmailAccount(emailAdressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull! " + emailAccount);
+                     viewFactory.showMainWindow();
+                     Stage stage = (Stage) errorLabel.getScene().getWindow();
+                     viewFactory.closeStage(stage);
+                    return;
+            }
+        }
+
+
+    }
+    private boolean fieldsAreValid() {
+       if(emailAdressField.getText().isEmpty()){
+           errorLabel.setText("Please fill email");
+           return false;
+       }
+        if(passwordField.getText().isEmpty()){
+            errorLabel.setText("Please fill password");
+            return false;
+        }
+        return true;
     }
 
 }
